18c81b431d3f19f9e1f371667b78519de3f2ac05
Olivier Girard <olgirard@gmail.com>
2011-06-23 19:30:51 +0000

    To facilitate commercial adoption of the openMSP430, the core has moved to a modified BSD license.
    git-svn-id: http://opencores.org/ocsvn/openmsp430/openmsp430/trunk@117 c36ddccb-d6af-41aa-abd5-588c0c1d0201

--- core/rtl/verilog/periph/template_periph_8b.v
+++ core/rtl/verilog/periph/template_periph_8b.v
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-// Copyright (C) 2009 Authors
+// Copyright (C) 2009 , Olivier Girard
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
